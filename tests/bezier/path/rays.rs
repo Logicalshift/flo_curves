e084d6071b2ecb179928fdebec7109e7db7d6724
Andrew Hunter <andrew@logicalshift.co.uk>
2019-05-17 23:50:46 +0100

    Try reversing the direction of one of the two triangles for the figure-of-8 collision
    ... This situation might be our failure too (in a similar case we're generating 3 collisions instead of 2) but it seems to be reliably producing 2 here.

--- tests/bezier/path/rays.rs
+++ tests/bezier/path/rays.rs
@@ -7,8 +7,8 @@ fn crossing_figure_of_8_intersection_from_inside() {
     // +       +
     // | \   / |
     // |   +   | <--- RAY
-    // | /  \  |
-    // +      +
+    // | /   \ |
+    // +       +
     // 
     // This ray hits a corner but it should generate either 0 or 2 collisions at this point, and particularly not 1.
     // (0 intersections implies the ray never leaves the shape and 2 intersections indicates it leaves and immediately 
@@ -66,7 +66,57 @@ fn crossing_figure_of_8_intersection_from_inside() {
     assert!(collisions.len() != 3);
     assert!((collisions.len()&1) == 0);
 
+    assert!(collisions.len() == 4);
+}
+
+#[test]
+fn crossing_figure_of_8_intersection_from_inside_reversed() {
+    //
+    // +       +
+    // | \   / |
+    // |   +   | <--- RAY
+    // | /   \ |
+    // +       +
+    // 
+
+    let left_triangle = BezierPathBuilder::<SimpleBezierPath>::start(Coord2(1.0, 1.0))
+        .line_to(Coord2(1.0, 3.0))
+        .line_to(Coord2(3.0, 2.0))
+        .line_to(Coord2(1.0, 1.0))
+        .build();
+    let right_triangle = BezierPathBuilder::<SimpleBezierPath>::start(Coord2(6.0, 3.0))
+        .line_to(Coord2(3.0, 2.0))
+        .line_to(Coord2(6.0, 1.0))
+        .line_to(Coord2(6.0, 3.0))
+        .build();
+
+    let graph_path = GraphPath::from_path(&left_triangle, ());
+    let graph_path = graph_path.collide(GraphPath::from_path(&right_triangle, ()), 0.01);
+
+    let collisions = graph_path.ray_collisions(&(Coord2(8.0, 2.0), Coord2(7.0, 2.0)));
+
+    assert!(collisions.len() != 3);
+    assert!((collisions.len()&1) == 0);
+
     assert!(collisions.len() == 4 || collisions.len() == 2);
+
+    // The intersection point should be an actual intersection
+    assert!((0..(graph_path.num_points())).into_iter()
+        .map(|point_idx| graph_path.edges_for_point(point_idx).count())
+        .filter(|num_edges_for_point| num_edges_for_point == &2)
+        .count() == 1);
+    assert!((0..(graph_path.num_points())).into_iter()
+        .map(|point_idx| graph_path.edges_for_point(point_idx).count())
+        .filter(|num_edges_for_point| num_edges_for_point == &1)
+        .count() == 4);
+
+    // Also test the ray travelling the other way
+    let collisions = graph_path.ray_collisions(&(Coord2(-2.0, 2.0), Coord2(-1.0, 2.0)));
+
+    assert!(collisions.len() != 3);
+    assert!((collisions.len()&1) == 0);
+
+    assert!(collisions.len() == 4);
 }
 
 #[test]
@@ -75,27 +125,9 @@ fn crossing_figure_of_8_intersection_from_inside_nearby() {
     // +       +
     // | \   / |
     // |   +   | <--- RAY
-    // | /  \  |
-    // +      +
+    // | /   \ |
+    // +       +
     // 
-    // This ray hits a corner but it should generate either 0 or 2 collisions at this point, and particularly not 1.
-    // (0 intersections implies the ray never leaves the shape and 2 intersections indicates it leaves and immediately 
-    // re-enters)
-    // 
-    // (Interestingly, either behaviour is correct: if there are 0 collisions we won't categorise the edges and if there
-    // are 2 we'll mark the edges as exterior when the ray is used to set edge kinds)
-    // 
-    // This is interesting because of this case:
-    // 
-    // +
-    // | \
-    // |   +  <--- RAY
-    // | /
-    // +
-    // 
-    // As the 'same' point here should always generate 1 intersection as the ray enters the shape at this point (or leaves
-    // in the reverse direction)
-    //
 
     let left_triangle = BezierPathBuilder::<SimpleBezierPath>::start(Coord2(1.0, 1.0))
         .line_to(Coord2(1.0, 3.0))
@@ -135,7 +167,7 @@ fn crossing_figure_of_8_intersection_from_inside_nearby() {
         assert!(collisions.len() != 3);
         assert!((collisions.len()&1) == 0);
 
-        assert!(collisions.len() == 4 || collisions.len() == 2);
+        assert!(collisions.len() == 4);
     }
 }
 
