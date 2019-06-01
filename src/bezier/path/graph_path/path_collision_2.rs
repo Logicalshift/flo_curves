a8cfdcf4e927b74b2a6a135762c1692744133247
Andrew Hunter <andrew@logicalshift.co.uk>
2019-06-01 23:26:17 +0100

    Filter out the collisions where two edges join anyway

--- src/bezier/path/graph_path/path_collision_2.rs
+++ src/bezier/path/graph_path/path_collision_2.rs
@@ -85,8 +85,9 @@ impl<Point: Coordinate+Coordinate2D, Label: Copy> GraphPath<Point, Label> {
                             // Remove any pairs of collisions that are too close together
                             remove_and_round_close_collisions(&mut edge_collisions, &src_curve, &tgt_curve);
 
-                            // Turn into collisions
+                            // Turn into collisions, filtering out the collisions that occur at the ends (where one edge joins another)
                             let edge_collisions = edge_collisions.into_iter()
+                                .filter(|(src_t, tgt_t)| !((Self::t_is_one(*src_t) || Self::t_is_zero(*src_t)) && (Self::t_is_one(*tgt_t) || Self::t_is_zero(*tgt_t))))
                                 .map(|(src_t, tgt_t)| {
                                     Collision {
                                         edge_1:     src_curve_ref,
