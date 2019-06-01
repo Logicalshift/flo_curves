434e5ff3b7b7a3d1a5f156c52c72945439d1f171
Andrew Hunter <andrew@logicalshift.co.uk>
2019-06-01 18:23:39 +0100

    Add a way to generate following edge indexes

--- src/bezier/path/graph_path/edge_ref.rs
+++ src/bezier/path/graph_path/edge_ref.rs
@@ -1,4 +1,4 @@
-use super::{GraphEdge,GraphEdgeRef};
+use super::{GraphPath, GraphEdge, GraphEdgeRef};
 use super::super::super::super::geo::*;
 
 impl GraphEdgeRef {
@@ -28,3 +28,38 @@ impl<'a, 'b, Point: 'a+Coordinate, Label: 'a+Copy> From<&'b GraphEdge<'a, Point,
         edge.edge
     }
 }
+
+impl<Point: Coordinate+Coordinate2D, Label> GraphPath<Point, Label> {
+    ///
+    /// Given an edge ref, returns the edge ref that follows it
+    ///
+    #[inline]
+    pub fn following_edge_ref(&self, edge_ref: GraphEdgeRef) -> GraphEdgeRef {
+        if edge_ref.reverse {
+            // Need to search in reverse for the edge
+            for connected_from in self.points[edge_ref.start_idx].connected_from.iter() {
+                for (edge_idx, edge) in self.points[*connected_from].forward_edges.iter().enumerate() {
+                    if edge.end_idx == edge_ref.start_idx {
+                        return GraphEdgeRef {
+                            start_idx:  *connected_from,
+                            edge_idx:   edge_idx,
+                            reverse:    true
+                        }
+                    }
+                }
+            }
+
+            panic!("Reverse edge could not be found")
+        } else {
+            // Can just use the following edge
+            let edge = &self.points[edge_ref.start_idx].forward_edges[edge_ref.edge_idx];
+
+            GraphEdgeRef {
+                start_idx:  edge.end_idx,
+                edge_idx:   edge.following_edge_idx,
+                reverse:    false
+            }
+        }
+    }
+}
+
