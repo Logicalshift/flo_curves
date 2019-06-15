1a9be0dc5a5a3bd4d750998abce96f07ec38eaf4
Andrew Hunter <andrew@logicalshift.co.uk>
2019-06-15 14:37:14 +0100

    Another place we can use smallvecs

--- src/bezier/solve.rs
+++ src/bezier/solve.rs
@@ -3,6 +3,7 @@ use super::super::geo::*;
 use super::super::consts::*;
 
 use roots::{find_roots_cubic, Roots};
+use smallvec::*;
 
 const CLOSE_ENOUGH: f64 = SMALL_DISTANCE * 50.0;
 
@@ -10,7 +11,7 @@ const CLOSE_ENOUGH: f64 = SMALL_DISTANCE * 50.0;
 /// Solves for t in a single dimension for a bezier curve (finds the point(s) where the basis
 /// function evaluates to p)
 /// 
-pub fn solve_basis_for_t(w1: f64, w2: f64, w3: f64, w4: f64, p: f64) -> Vec<f64> {
+pub fn solve_basis_for_t(w1: f64, w2: f64, w3: f64, w4: f64, p: f64) -> SmallVec<[f64; 4]> {
     // Compute the coefficients for the cubic bezier function
     let d = w1-p;
     let c = 3.0*(w2-w1);
@@ -20,11 +21,11 @@ pub fn solve_basis_for_t(w1: f64, w2: f64, w3: f64, w4: f64, p: f64) -> Vec<f64>
     // Solve for p
     let roots = find_roots_cubic(a, b, c, d);
     let mut roots = match roots {
-        Roots::No(_)    => vec![],
-        Roots::One(r)   => r.to_vec(),
-        Roots::Two(r)   => r.to_vec(),
-        Roots::Three(r) => r.to_vec(),
-        Roots::Four(r)  => r.to_vec()
+        Roots::No(_)                => smallvec![],
+        Roots::One([a])             => smallvec![a],
+        Roots::Two([a, b])          => smallvec![a, b],
+        Roots::Three([a, b, c])     => smallvec![a, b, c],
+        Roots::Four([a, b, c, d])   => smallvec![a, b, c, d]
     };
 
     // Clip to 0/1 for small ranges outside
@@ -34,7 +35,7 @@ pub fn solve_basis_for_t(w1: f64, w2: f64, w3: f64, w4: f64, p: f64) -> Vec<f64>
     }
 
     // Remove any roots outside the range of the function
-    roots.retain(|r| r >= &0.0 && r <= &1.0);
+    roots.retain(|r| *r >= 0.0 && *r <= 1.0);
 
     // Return the roots
     roots
