d22a9141e2c73f694e9e5e607c170b6b98f6033c
Andrew Hunter <andrew@logicalshift.co.uk>
2020-04-27 09:39:34 +0100

    Add a function for generating our ray-cast function and add an independent test
    It seems to be malfunctioning

--- tests/bezier/algorithms/fill_concave.rs
+++ tests/bezier/algorithms/fill_concave.rs
@@ -3,12 +3,8 @@ use flo_curves::bezier::*;
 use flo_curves::bezier::path::*;
 use flo_curves::bezier::path::algorithms::*;
 
-#[test]
-fn fill_concave_circle() {
-    // Simple circle ray-casting algorithm
-    let circle_center   = Coord2(10.0, 10.0);
-    let radius          = 5.0;
-    let circle_ray_cast = |from: Coord2, to: Coord2| {
+fn circle_ray_cast(circle_center: Coord2, radius: f64) -> impl Fn(Coord2, Coord2) -> Vec<RayCollision<Coord2, ()>> {
+    move |from: Coord2, to: Coord2| {
         let from    = from - circle_center;
         let to      = to - circle_center;
 
@@ -31,7 +27,42 @@ fn fill_concave_circle() {
         vec![
             RayCollision::new(Coord2(xc1, yc1)+circle_center, ()), RayCollision::new(Coord2(xc2, yc2)+circle_center, ())
         ]
-    };
+    }
+}
+
+#[test]
+fn ray_cast_to_circle() {
+    let ray_cast    = circle_ray_cast(Coord2(10.0, 10.0), 5.0);
+
+    let from_center = ray_cast(Coord2(10.0, 10.0), Coord2(11.0, 11.0));
+
+    assert!(from_center.len() == 2);
+    assert!((from_center[0].position.distance_to(&Coord2(10.0, 10.0))-5.0).abs() < 0.1);
+    assert!((from_center[1].position.distance_to(&Coord2(10.0, 10.0))-5.0).abs() < 0.1);
+    assert!(from_center[0].position.distance_to(&Coord2(13.54, 13.54)) < 0.1);
+    assert!(from_center[1].position.distance_to(&Coord2(6.46, 6.46)) < 0.1);
+
+    let offset = ray_cast(Coord2(11.0, 11.0), Coord2(12.0, 12.0));
+
+    assert!(offset.len() == 2);
+    assert!((offset[0].position.distance_to(&Coord2(10.0, 10.0))-5.0).abs() < 0.1);
+    assert!((offset[1].position.distance_to(&Coord2(10.0, 10.0))-5.0).abs() < 0.1);
+    assert!(offset[0].position.distance_to(&Coord2(13.54, 13.54)) < 0.1);
+    assert!(offset[1].position.distance_to(&Coord2(6.46, 6.46)) < 0.1);
+
+    let offset2 = ray_cast(Coord2(11.0, 11.0), Coord2(12.0, 11.0));
+
+    assert!(offset2.len() == 2);
+    assert!((offset2[0].position.distance_to(&Coord2(10.0, 10.0))-5.0).abs() < 0.1);
+    assert!((offset2[1].position.distance_to(&Coord2(10.0, 10.0))-5.0).abs() < 0.1);
+}
+
+#[test]
+fn fill_concave_circle() {
+    // Simple circle ray-casting algorithm
+    let circle_center   = Coord2(10.0, 10.0);
+    let radius          = 5.0;
+    let circle_ray_cast = circle_ray_cast(circle_center, radius);
 
     // Flood-fill this curve
     let path = flood_fill_concave::<SimpleBezierPath, _, _, _,_>(circle_center, &FillSettings::default(), circle_ray_cast);
