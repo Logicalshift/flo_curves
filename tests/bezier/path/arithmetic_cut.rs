036c5a580360b191c16b975155c56b137bf7df0c
Andrew Hunter <andrew@logicalshift.co.uk>
2020-10-31 15:00:42 +0000

    Add some more tests for the 'cut' routine that look at different ways a square can be cut up

--- tests/bezier/path/arithmetic_cut.rs
+++ tests/bezier/path/arithmetic_cut.rs
@@ -25,3 +25,78 @@ fn cut_square() {
     assert!(cut_square.interior_path[0].points().len() == 4);
     assert!(cut_square.exterior_path[0].points().len() == 6);
 }
+
+#[test]
+fn cut_square_entirely_interior() {
+    let square_1    = BezierPathBuilder::<SimpleBezierPath>::start(Coord2(5.0, 5.0))
+        .line_to(Coord2(10.0, 5.0))
+        .line_to(Coord2(10.0, 10.0))
+        .line_to(Coord2(5.0, 10.0))
+        .line_to(Coord2(5.0, 5.0))
+        .build();
+
+    let square_2    = BezierPathBuilder::<SimpleBezierPath>::start(Coord2(2.0, 2.0))
+        .line_to(Coord2(15.0, 2.0))
+        .line_to(Coord2(15.0, 15.0))
+        .line_to(Coord2(2.0, 15.0))
+        .line_to(Coord2(2.0, 2.0))
+        .build();
+
+    let cut_square  = path_cut::<_, _, SimpleBezierPath>(&vec![square_1], &vec![square_2], 0.01);
+
+    assert!(cut_square.exterior_path.len() == 0);
+    assert!(cut_square.interior_path.len() == 1);
+
+    assert!(cut_square.interior_path[0].points().len() == 4);
+}
+
+
+#[test]
+fn cut_square_entirely_exterior() {
+    let square_1    = BezierPathBuilder::<SimpleBezierPath>::start(Coord2(5.0, 5.0))
+        .line_to(Coord2(10.0, 5.0))
+        .line_to(Coord2(10.0, 10.0))
+        .line_to(Coord2(5.0, 10.0))
+        .line_to(Coord2(5.0, 5.0))
+        .build();
+
+    let square_2    = BezierPathBuilder::<SimpleBezierPath>::start(Coord2(20.0, 20.0))
+        .line_to(Coord2(15.0, 20.0))
+        .line_to(Coord2(15.0, 15.0))
+        .line_to(Coord2(20.0, 15.0))
+        .line_to(Coord2(20.0, 20.0))
+        .build();
+
+    let cut_square  = path_cut::<_, _, SimpleBezierPath>(&vec![square_1], &vec![square_2], 0.01);
+
+    assert!(cut_square.exterior_path.len() == 1);
+    assert!(cut_square.interior_path.len() == 0);
+
+    assert!(cut_square.exterior_path[0].points().len() == 4);
+}
+
+#[test]
+fn cut_square_center() {
+    let square_1    = BezierPathBuilder::<SimpleBezierPath>::start(Coord2(5.0, 5.0))
+        .line_to(Coord2(10.0, 5.0))
+        .line_to(Coord2(10.0, 10.0))
+        .line_to(Coord2(5.0, 10.0))
+        .line_to(Coord2(5.0, 5.0))
+        .build();
+
+    let square_2    = BezierPathBuilder::<SimpleBezierPath>::start(Coord2(6.0, 6.0))
+        .line_to(Coord2(9.0, 6.0))
+        .line_to(Coord2(9.0, 9.0))
+        .line_to(Coord2(6.0, 9.0))
+        .line_to(Coord2(6.0, 6.0))
+        .build();
+
+    let cut_square  = path_cut::<_, _, SimpleBezierPath>(&vec![square_1], &vec![square_2], 0.01);
+
+    assert!(cut_square.exterior_path.len() == 2);
+    assert!(cut_square.interior_path.len() == 1);
+
+    assert!(cut_square.interior_path[0].points().len() == 4);
+    assert!(cut_square.exterior_path[0].points().len() == 4);
+    assert!(cut_square.exterior_path[1].points().len() == 4);
+}
