8f61011d4541f799629e58361b8089bbb9bf6e5a
Andrew Hunter <andrew@logicalshift.co.uk>
2021-02-07 12:15:07 +0000

    Test many values of t for the diagonal case in case it makes a difference (could cause loss of precision with small enough values?)

--- tests/bezier/normal.rs
+++ tests/bezier/normal.rs
@@ -26,17 +26,20 @@ fn normal_for_vert_short_line_is_straight_up() {
 }
 
 #[test]
-fn normal_for_vert_short_diagonal_line_is_straight_up() {
+fn normal_for_vert_short_diagonal_line_is_diagonal() {
     let line    = bezier::Curve::from_points(Coord2(0.0,0.0), (Coord2(0.0000000003, 0.0000000003), Coord2(0.0000000007, 0.0000000007)), Coord2(0.0000000010, 0.0000000010));
-    let normal  = line.normal_at_pos(0.5);
+    for t in 0..100 {
+        let t       = (t as f64) / 100.0;
+        let normal  = line.normal_at_pos(t);
 
-    // Normals usually aren't unit vectors, but will produce very small values for very short lines
-    let normal = normal.to_unit_vector();
+        // Normals usually aren't unit vectors, but will produce very small values for very short lines
+        let normal = normal.to_unit_vector();
 
-    // Normal should be a 45 degree diagonal line
-    assert!(normal.x() < -0.01);
-    assert!(normal.y() > 0.01);
-    assert!((-normal.x()-normal.y()).abs() < 0.01);
+        // Normal should be a 45 degree diagonal line
+        assert!(normal.x() < -0.01);
+        assert!(normal.y() > 0.01);
+        assert!((-normal.x()-normal.y()).abs() < 0.01);
+    }
 }
 
 #[test]
