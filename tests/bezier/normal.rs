66c2b55a753784d51ea01d2d47c04d471708b554
Andrew Hunter <andrew@logicalshift.co.uk>
2021-02-07 12:07:13 +0000

    Add some more tests for very short curves and check the behaviour when a line is a point

--- tests/bezier/normal.rs
+++ tests/bezier/normal.rs
@@ -1,6 +1,6 @@
 use flo_curves::*;
 use flo_curves::bezier;
-use flo_curves::bezier::NormalCurve;
+use flo_curves::bezier::{NormalCurve};
 
 #[test]
 fn normal_for_line_is_straight_up() {
@@ -14,12 +14,39 @@ fn normal_for_line_is_straight_up() {
 
 #[test]
 fn normal_for_vert_short_line_is_straight_up() {
-    let line    = bezier::Curve::from_points(Coord2(0.0,0.0), (Coord2(0.000003, 0.0), Coord2(0.000007, 0.0)), Coord2(0.000010, 0.0));
+    let line    = bezier::Curve::from_points(Coord2(0.0,0.0), (Coord2(0.0000000003, 0.0), Coord2(0.0000000007, 0.0)), Coord2(0.0000000010, 0.0));
     let normal  = line.normal_at_pos(0.5);
+
+    // Normals usually aren't unit vectors, but will produce very small values for very short lines
+    let normal = normal.to_unit_vector();
 
     // Normal should be a line facing up
     assert!(normal.x().abs() < 0.01);
     assert!(normal.y() > 0.01);
+}
+
+#[test]
+fn normal_for_vert_short_diagonal_line_is_straight_up() {
+    let line    = bezier::Curve::from_points(Coord2(0.0,0.0), (Coord2(0.0000000003, 0.0000000003), Coord2(0.0000000007, 0.0000000007)), Coord2(0.0000000010, 0.0000000010));
+    let normal  = line.normal_at_pos(0.5);
+
+    // Normals usually aren't unit vectors, but will produce very small values for very short lines
+    let normal = normal.to_unit_vector();
+
+    // Normal should be a 45 degree diagonal line
+    assert!(normal.x() < -0.01);
+    assert!(normal.y() > 0.01);
+    assert!((-normal.x()-normal.y()).abs() < 0.01);
+}
+
+#[test]
+fn normal_for_point() {
+    let line    = bezier::Curve::from_points(Coord2(0.0,0.0), (Coord2(0.0, 0.0), Coord2(0.0, 0.0)), Coord2(0.0, 0.0));
+    let normal  = line.normal_at_pos(0.5);
+
+    // Normal should be the (0,0) vector (points don't have normals)
+    assert!(normal.x().abs() < 0.0001);
+    assert!(normal.y() < 0.0001);
 }
 
 #[test]
