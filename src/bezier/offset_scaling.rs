0c9da09a778ef4d417555a5716fff7a8e1985f45
Andrew Hunter <andrew@logicalshift.co.uk>
2021-02-26 20:30:22 +0000

    Add the main offset() function back into offset.rs

--- src/bezier/offset_scaling.rs
+++ src/bezier/offset_scaling.rs
@@ -36,15 +36,6 @@ use smallvec::*;
 ///
 /// Computes a series of curves that approximate an offset curve from the specified origin curve.
 ///
-pub fn offset<Curve>(curve: &Curve, initial_offset: f64, final_offset: f64) -> Vec<Curve>
-where   Curve:          BezierCurveFactory+NormalCurve,
-        Curve::Point:   Normalize+Coordinate2D {
-    offset_scaling(curve, initial_offset, final_offset)
-}
-
-///
-/// Computes a series of curves that approximate an offset curve from the specified origin curve.
-///
 /// This uses a scaling algorithm to compute the offset curve, which is fast but which can produce
 /// errors, especially if the initial and final offsets are very different from one another.
 ///
