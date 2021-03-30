b02b6c76c3c05af4ddb3e0d9a0ca318b94b15067
Andrew Hunter <andrew@logicalshift.co.uk>
2021-03-30 13:35:19 +0100

    Write out the curve sections from the distort example for where they're useful

--- tests/bezier/distort.rs
+++ tests/bezier/distort.rs
@@ -7,6 +7,8 @@ fn line_to_sine_wave() {
     let distorted   = distort_curve::<_, _, Curve<_>>(&line, |pos, _t| Coord2(pos.x(), pos.y() + (pos.x()*20.0).sin()), 1.0, 1.0).expect("Fit curve");
 
     for curve in distorted.into_iter() {
+        println!("{:?}", curve);
+
         for section in walk_curve_evenly(&curve, 1.0, 0.1) {
             let (t_min, t_max)  = section.original_curve_t_values();
             let t_mid           = (t_min+t_max)/2.0;
@@ -15,7 +17,7 @@ fn line_to_sine_wave() {
             let expected_y      = 100.0 + (pos.x()*20.0).sin();
             let actual_y        = pos.y();
 
-            println!("{:?} {:?} {:?} {:?}", t_mid, expected_y, actual_y, (expected_y-actual_y).abs());
+            println!("  {:?} {:?} {:?} {:?}", t_mid, expected_y, actual_y, (expected_y-actual_y).abs());
 
             assert!((expected_y-actual_y).abs() < 4.0);
         }
