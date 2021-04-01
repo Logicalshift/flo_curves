a7b6a9a24a3d5daa067109c77530b70fc10b28fc
Andrew Hunter <andrew@logicalshift.co.uk>
2021-04-01 11:16:23 +0100

    First pass at an implementation of the 'sweep against' iterator implementation

--- src/geo/sweep.rs
+++ src/geo/sweep.rs
@@ -34,11 +34,11 @@ TgtBoundsIter:  'a+Iterator<Item=&'a TItem>,
 TItem:          'a+HasBoundingBox,
 TItem::Point:   Coordinate2D {
     SweepAgainstIterator {
-        src_iterator:   src,
+        src_iterator:   Some(src),
         tgt_iterator:   tgt,
         pending:        smallvec![],
         src_by_max_x:   Vec::new(),
-        tgt_by_max_x:   Vec::new()
+        src_last_min_x: f64::MIN
     }
 }
 
@@ -141,19 +141,19 @@ TgtIterator:    'a+Iterator<Item=&'a TItem>,
 TItem:          'a+HasBoundingBox,
 TItem::Point:   Coordinate2D {
     /// Iterator, ordered by minimum X position
-    src_iterator: SrcIterator,
+    src_iterator: Option<SrcIterator>,
 
     /// Iterator, ordered by minimum X position, that returns the items to be checked for overlaps
     tgt_iterator: TgtIterator,
+
+    /// The min_x position of the last src item read (or f64::MIN)
+    src_last_min_x: f64,
 
     /// Collided items that are pending a return
     pending: SmallVec<[(&'a TItem, &'a TItem); 16]>,
 
     /// Source items that have not yet been swept away, ordered by maximum x position (in reverse, so the next item to remove can be popped)
     src_by_max_x: Vec<(Bounds<TItem::Point>, &'a TItem)>,
-
-    /// Target items that have npt yet been swept away, ordered by maximum x position (in reverse, so the next item to remove can be popped)
-    tgt_by_max_x: Vec<(Bounds<TItem::Point>, &'a TItem)>
 }
 
 impl<'a, TItem, SrcIterator, TgtIterator> Iterator for SweepAgainstIterator<'a, TItem, SrcIterator, TgtIterator>
@@ -165,6 +165,72 @@ TItem::Point:   Coordinate2D {
     type Item = (&'a TItem, &'a TItem);
 
     fn next(&mut self) -> Option<Self::Item> {
-        None
+        loop {
+            // Return any pending collision (if there are any)
+            if let Some(pending) = self.pending.pop() {
+                return Some(pending);
+            }
+
+            // Read a new target item. Target items determine the sweep position (we read things in order such that there'll be no collisions before this point)
+            let next_tgt        = self.tgt_iterator.next();
+            let next_tgt        = if let Some(next_tgt) = next_tgt { next_tgt } else { return None };
+
+            let next_tgt_bounds = next_tgt.get_bounding_box::<Bounds<_>>();
+
+            // Sweep the source and target items
+            let tgt_min_x       = next_tgt_bounds.min().x();
+            let tgt_max_x       = next_tgt_bounds.max().x();
+
+            while let Some((earliest_x, _item)) = self.src_by_max_x.last() {
+                if earliest_x.max().x() >= tgt_min_x {
+                    break;
+                }
+
+                self.src_by_max_x.pop();
+            }
+
+            // Read source items and add them to the src list until we find one after the existing target
+            loop {
+                // Stop reading if we get a source item that can't overlap the current target item
+                if self.src_last_min_x > tgt_max_x { break; }
+
+                // Try to read the next source item
+                let next_src = if let Some(next_src) = self.src_iterator.as_mut().and_then(|iter| iter.next()) {
+                    next_src
+                } else {
+                    self.src_iterator = None;
+                    break; 
+                };
+
+                // Add to the list of source items
+                let src_bounds  = next_src.get_bounding_box::<Bounds<_>>();
+                let src_min_x   = src_bounds.min().x();
+                let src_max_x   = src_bounds.max().x();
+
+                let index   = self.src_by_max_x.binary_search_by(|(bounds, _item)| {
+                    let item_max_x = bounds.max().x();
+
+                    if item_max_x > src_max_x {
+                        Ordering::Less
+                    } else if item_max_x == src_max_x {
+                        Ordering::Equal
+                    } else {
+                        Ordering::Greater
+                    }
+                }).unwrap_or_else(|idx| idx);
+
+                self.src_by_max_x.insert(index, (src_bounds, next_src));
+
+                // Set as the min x last seen from the source
+                self.src_last_min_x = src_min_x;
+            }
+
+            // Collide the source item list with the current item to fill the pending array
+            for (src_item_bounds, src_item) in self.src_by_max_x.iter() {
+                if src_item_bounds.overlaps(&next_tgt_bounds) {
+                    self.pending.push((src_item, next_tgt));
+                }
+            }
+        }
     }
 }
