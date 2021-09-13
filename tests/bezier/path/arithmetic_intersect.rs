5862198f88df6539ba0b2f486bcc2cbf06dbde9e
Andrew Hunter <andrew@logicalshift.co.uk>
2021-09-13 09:46:15 +0100

    One thing Flowbetween does is downconvert the remaining path between slices, and then convert it back to 64-bits. This seems to reproduce the bug where we get a 'bad' slice
    (Seems likely we get a path that should intersect but does not)

--- tests/bezier/path/arithmetic_intersect.rs
+++ tests/bezier/path/arithmetic_intersect.rs
@@ -175,3 +175,84 @@ fn repeatedly_full_intersect_circle() {
         assert!((distance_to_center-0.0).abs() < 0.1 || (distance_to_center-16.0).abs() < 1.0);
     }
 }
+
+fn convert_path_to_f32_and_back((start_point, remaining_points): SimpleBezierPath) -> SimpleBezierPath {
+    let start_f32       = (start_point.x() as f32, start_point.y() as f32);
+    let remaining_f32   = remaining_points.into_iter().map(|(cp1, cp2, p)| {
+        ((cp1.x() as f32, cp1.y() as f32), (cp2.x() as f32, cp2.y() as f32), (p.x() as f32, p.y() as f32))
+    });
+
+    let path_points = remaining_f32.map(|(cp1, cp2, p)| {
+        (Coord2(cp1.0 as f64, cp1.1 as f64), Coord2(cp2.0 as f64, cp2.1 as f64), Coord2(p.0 as f64, p.1 as f64))
+    });
+    (Coord2(start_f32.0 as _, start_f32.1 as _), path_points.collect())
+}
+
+#[test]
+fn repeatedly_full_intersect_circle_f32_intermediate_representation() {
+    // Start with a circle
+    let circle          = Circle::new(Coord2(500.0, 500.0), 116.0).to_path::<SimpleBezierPath>();
+
+    // Cut 16 triangular slices from it
+    let mut remaining   = vec![circle];
+    let mut slices      = vec![];
+
+    for slice_idx in 0..16 {
+        // Angle in radians of this slice
+        let middle_angle            = f64::consts::PI*2.0 / 16.0 * (slice_idx as f64);
+        let start_angle             = middle_angle - (f64::consts::PI*2.0 / 32.0);
+        let end_angle               = middle_angle + (f64::consts::PI*2.0 / 32.0);
+
+        // Create a triangle slice
+        let (center_x, center_y)    = (500.0, 500.0);
+        let (x1, y1)                = (center_x + (f64::sin(start_angle) * 300.0),  center_y + (f64::cos(start_angle) * 300.0));
+        let (x2, y2)                = (center_x + (f64::sin(end_angle) * 300.0),    center_y + (f64::cos(end_angle) * 300.0));
+        let (x3, y3)                = (center_x + (f64::sin(start_angle) * 16.0),    center_y + (f64::cos(start_angle) * 16.0));
+        let (x4, y4)                = (center_x + (f64::sin(end_angle) * 16.0),      center_y + (f64::cos(end_angle) * 16.0));
+
+        let fragment                = BezierPathBuilder::<SimpleBezierPath>::start(Coord2(x3, y3))
+            .line_to(Coord2(x1, y1))
+            .line_to(Coord2(x2, y2))
+            .line_to(Coord2(x4, y4))
+            .line_to(Coord2(x3, y3))
+            .build();
+
+        // Cut the circle via the fragment
+        let cut_circle              = path_full_intersect::<_, _, SimpleBezierPath>(&vec![fragment], &remaining, 0.01);
+
+        // Add the slice and the remaining part of the circle
+        slices.push(cut_circle.intersecting_path);
+        remaining = cut_circle.exterior_paths[1].clone();
+
+        // Reduce and re-increase the precision of the remaining path (this happens in FlowBetween: even though the points will be in slightly different positions we should still be able to slice using this curve)
+        remaining = remaining.into_iter().map(|path| convert_path_to_f32_and_back(path)).collect();
+    }
+
+    // Each fragment should consist of points that are either at the origin or on the circle
+    for circle_fragment in slices {
+        assert!(circle_fragment.len() == 1);
+
+        let start_point = circle_fragment[0].start_point();
+        let points      = circle_fragment[0].points().map(|(_, _, p)| p);
+        let all_points  = iter::once(start_point).chain(points);
+
+        for circle_point in all_points {
+            let distance_to_center = circle_point.distance_to(&Coord2(500.0, 500.0));
+            println!("{:?}", distance_to_center);
+            assert!((distance_to_center-16.0).abs() < 0.1 || (distance_to_center-116.0).abs() < 1.0);
+        }
+    }
+
+    // Should be a 16x16 polygon left over for the circle
+    assert!(remaining.len() == 1);
+
+    let start_point = remaining[0].start_point();
+    let points      = remaining[0].points().map(|(_, _, p)| p);
+    let all_points  = iter::once(start_point).chain(points);
+
+    for circle_point in all_points {
+        let distance_to_center = circle_point.distance_to(&Coord2(500.0, 500.0));
+        println!("{:?}", distance_to_center);
+        assert!((distance_to_center-0.0).abs() < 0.1 || (distance_to_center-16.0).abs() < 1.0);
+    }
+}
