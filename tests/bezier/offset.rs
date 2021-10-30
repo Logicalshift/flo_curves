96d6c08c64aad6b449306c47bf5bed7f94cb78d1
Andrew Hunter <andrew@logicalshift.co.uk>
2021-10-30 22:38:42 +0100

    Add a test for the LMS offsetting curve that checks the tangents at the start and end of the generated curve

--- tests/bezier/offset.rs
+++ tests/bezier/offset.rs
@@ -208,3 +208,43 @@ fn normals_for_line_do_not_meet_at_intersection() {
     let intersection    = line::ray_intersects_ray(&(start, start+start_normal), &(end, end+end_normal));
     assert!(intersection.is_none());
 }
+
+#[test]
+fn offset_lms_sampling_arc_start_tangent() {
+    use flo_curves::arc::*;
+
+    // 90 degree circle arc
+    let circle      = Circle::new(Coord2(0.0, 0.0), 100.0);
+    let arc         = circle.arc(0.0, f64::consts::PI / 2.0);
+    let arc_curve   = arc.to_bezier_curve::<Curve<Coord2>>();
+
+    // Offset by 10
+    let offset_arc  = offset_lms_sampling(&arc_curve, |_t| 10.0, 20, 0.01).expect("Offset curve");
+
+    let start_tangent_original  = arc_curve.tangent_at_pos(0.0).to_unit_vector();
+    let start_tangent_new       = offset_arc[0].tangent_at_pos(0.0).to_unit_vector();
+
+    println!("{:?} {:?}", start_tangent_original, start_tangent_new);
+
+    assert!(start_tangent_original.distance_to(&start_tangent_new) < 0.01);
+}
+
+#[test]
+fn offset_lms_sampling_arc_end_tangent() {
+    use flo_curves::arc::*;
+
+    // 90 degree circle arc
+    let circle      = Circle::new(Coord2(0.0, 0.0), 100.0);
+    let arc         = circle.arc(0.0, f64::consts::PI / 2.0);
+    let arc_curve   = arc.to_bezier_curve::<Curve<Coord2>>();
+
+    // Offset by 10
+    let offset_arc  = offset_lms_sampling(&arc_curve, |_t| 10.0, 20, 0.01).expect("Offset curve");
+
+    let end_tangent_original    = arc_curve.tangent_at_pos(1.0).to_unit_vector();
+    let end_tangent_new         = offset_arc[offset_arc.len()-1].tangent_at_pos(1.0).to_unit_vector();
+
+    println!("{:?} {:?}", end_tangent_original, end_tangent_new);
+
+    assert!(end_tangent_original.distance_to(&end_tangent_new) < 0.01);
+}
