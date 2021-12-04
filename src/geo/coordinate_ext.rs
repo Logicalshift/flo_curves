3b7326175077615499baee3ec3823c12ef59f5b3
Andrew Hunter <andrew@logicalshift.co.uk>
2021-12-04 23:03:25 +0000

    For a general coordinate we can also add a unit_vector() function to return an x-axis unit vector
    (This complements the existing origin() function)

--- src/geo/coordinate_ext.rs
+++ src/geo/coordinate_ext.rs
@@ -1,6 +1,16 @@
 use crate::geo::coordinate::*;
 
 ///
+/// Extra functions provided for coordinate types
+///
+pub trait CoordinateExt {
+    ///
+    /// Creates a unit vector along the x axis
+    ///
+    fn unit_vector() -> Self;
+}
+
+///
 /// Extra functions introduced for 2D coordinate types
 ///
 pub trait Coordinate2DExt {
@@ -8,6 +18,16 @@ pub trait Coordinate2DExt {
     /// Creates a unit vector at an angle in radians measured from the x-axis
     ///
     fn unit_vector_at_angle(radians: impl Into<f64>) -> Self;
+}
+
+impl<T> CoordinateExt for T 
+where T: Coordinate {
+    fn unit_vector() -> Self {
+        let mut components  = vec![0.0; Self::len()];
+        components[0]       = 1.0;
+
+        Self::from_components(&components)
+    }
 }
 
 impl<T> Coordinate2DExt for T
