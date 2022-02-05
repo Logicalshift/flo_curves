ca25d7d7bcf658ab1adb14a0654ff71e81e02949
Andrew Hunter <andrew@logicalshift.co.uk>
2022-02-05 14:29:51 +0000

    Oops, creating windows at 60fps causes problems

--- demos/remove_interior_points/src/main.rs
+++ demos/remove_interior_points/src/main.rs
@@ -10,9 +10,11 @@ use std::time::{Duration, Instant};
 
 fn main() {
     with_2d_graphics(|| {
+        let canvas          = create_canvas_window("Remove interior points demonstration");
+
         loop {
             canvas.draw(|gc| {
-
+                gc.clear_canvas(Color::Rgba(1.0, 1.0, 1.0, 1.0));
             });
 
             thread::sleep(Duration::from_nanos(1_000_000_000 / 60));
