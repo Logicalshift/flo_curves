fba9990a32bc0f837342c937cb217c27bb5ddd0f
Andrew Hunter <andrew@logicalshift.co.uk>
2022-02-07 09:31:32 +0000

    Use reduce() instead of fold1

--- src/bezier/path/bounds.rs
+++ src/bezier/path/bounds.rs
@@ -3,24 +3,22 @@ use super::to_curves::*;
 use super::super::curve::*;
 use super::super::super::geo::*;
 
-use itertools::*;
-
 ///
 /// Finds the bounds of a path
 /// 
 pub fn path_bounding_box<P: BezierPath, Bounds: BoundingBox<Point=P::Point>>(path: &P) -> Bounds {
     path_to_curves(path)
         .map(|curve: Curve<P::Point>| curve.bounding_box())
-        .fold1(|first: Bounds, second| first.union_bounds(second))
+        .reduce(|first: Bounds, second| first.union_bounds(second))
         .unwrap_or_else(|| Bounds::from_min_max(P::Point::origin(), P::Point::origin()))
 }
 
 ///
-/// Finds the bounds of a path
+/// Finds the bounds of a path using the looser 'fast' algorithm
 /// 
 pub fn path_fast_bounding_box<P: BezierPath, Bounds: BoundingBox<Point=P::Point>>(path: &P) -> Bounds {
     path_to_curves(path)
         .map(|curve: Curve<P::Point>| curve.fast_bounding_box())
-        .fold1(|first: Bounds, second| first.union_bounds(second))
+        .reduce(|first: Bounds, second| first.union_bounds(second))
         .unwrap_or_else(|| Bounds::from_min_max(P::Point::origin(), P::Point::origin()))
 }
