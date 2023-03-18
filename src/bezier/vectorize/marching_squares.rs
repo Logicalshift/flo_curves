67e958d8de3ed45a55cd4d988347082ed7812416
Andrew Hunter <andrew@logicalshift.co.uk>
2023-03-18 22:54:19 +0000

    Add in the edge to the ascii art diagrams

--- src/bezier/vectorize/marching_squares.rs
+++ src/bezier/vectorize/marching_squares.rs
@@ -27,72 +27,72 @@ impl ContourCell {
             0 | 15 => { ConnectedEdges::None }
 
             /* *-o
-             * | |
+             * |/|
              * o-o */
             1  => { ConnectedEdges::One(ContourEdgePair(ContourEdge::left(), ContourEdge::top())) }
 
             /* o-*
-             * | |
+             * |\|
              * o-o */
             2  => { ConnectedEdges::One(ContourEdgePair(ContourEdge::top(), ContourEdge::right())) }
 
             /* *-*
-             * | |
+             * |-|
              * o-o */
             3  => { ConnectedEdges::One(ContourEdgePair(ContourEdge::left(), ContourEdge::right())) }
 
             /* o-o
-             * | |
+             * |\|
              * *-o */
             4  => { ConnectedEdges::One(ContourEdgePair(ContourEdge::left(), ContourEdge::bottom())) }
 
             /* *-o
-             * | |
+             * |||
              * *-o */
             5  => { ConnectedEdges::One(ContourEdgePair(ContourEdge::left(), ContourEdge::top())) }
 
-            /* o-*
-             * | |
-             * *-o */
+            /* o--*
+             * |\\|
+             * *--o */
             6  => { ConnectedEdges::Two(ContourEdgePair(ContourEdge::left(), ContourEdge::bottom()), ContourEdgePair(ContourEdge::top(), ContourEdge::right())) }
 
             /* *-*
-             * | |
+             * |/|
              * *-o */
             7  => { ConnectedEdges::One(ContourEdgePair(ContourEdge::bottom(), ContourEdge::right())) }
 
             /* o-o
-             * | |
+             * |/|
              * o-* */
             8  => { ConnectedEdges::One(ContourEdgePair(ContourEdge::bottom(), ContourEdge::right())) }
 
-            /* *-o
-             * | |
-             * o-* */
+            /* *--o
+             * |//|
+             * o--* */
             9  => { ConnectedEdges::Two(ContourEdgePair(ContourEdge::left(), ContourEdge::top()), ContourEdgePair(ContourEdge::bottom(), ContourEdge::right())) }
 
             /* o-*
-             * | |
+             * |||
              * o-* */
             10 => { ConnectedEdges::One(ContourEdgePair(ContourEdge::top(), ContourEdge::bottom())) }
 
             /* *-*
-             * | |
+             * |\|
              * o-* */
             11 => { ConnectedEdges::One(ContourEdgePair(ContourEdge::left(), ContourEdge::bottom())) }
 
             /* o-o
-             * | |
+             * |-|
              * *-* */
             12 => { ConnectedEdges::One(ContourEdgePair(ContourEdge::left(), ContourEdge::right())) }
 
             /* *-o
-             * | |
+             * |\|
              * *-* */
             13 => { ConnectedEdges::One(ContourEdgePair(ContourEdge::top(), ContourEdge::right())) }
 
             /* o-*
-             * | |
+             * |/|
              * *-* */
             14 => { ConnectedEdges::One(ContourEdgePair(ContourEdge::left(), ContourEdge::top())) }
 
