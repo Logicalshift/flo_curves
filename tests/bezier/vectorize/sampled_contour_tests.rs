28a73d48cb1083eb766899c72e2c3e8228351bb1
Andrew Hunter <andrew@logicalshift.co.uk>
2023-03-19 11:03:34 +0000

    Add a 'perimeter' test

--- tests/bezier/vectorize/sampled_contour_tests.rs
+++ tests/bezier/vectorize/sampled_contour_tests.rs
@@ -48,3 +48,37 @@ fn filled_iterator() {
             ContourPosition(3, 3),
         ], "Cell positions are {:?}", edge_cell_positions);
 }
+
+#[test]
+fn perimeter() {
+    // Two loops, one inner, one outer
+    let contour = U8SampledContour(ContourSize(3, 3), vec![
+            1, 1, 1,
+            1, 0, 1,
+            1, 1, 1,
+        ]);
+
+    let edge_cell_positions = contour.edge_cell_iterator().map(|(pos, _cell)| pos).collect::<Vec<_>>();
+
+    assert!(edge_cell_positions == vec![
+            ContourPosition(0, 0),
+            ContourPosition(1, 0),
+            ContourPosition(2, 0),
+            ContourPosition(3, 0),
+
+            ContourPosition(0, 1),
+            ContourPosition(1, 1),
+            ContourPosition(2, 1),
+            ContourPosition(3, 1),
+
+            ContourPosition(0, 2),
+            ContourPosition(1, 2),
+            ContourPosition(2, 2),
+            ContourPosition(3, 2),
+
+            ContourPosition(0, 3),
+            ContourPosition(1, 3),
+            ContourPosition(2, 3),
+            ContourPosition(3, 3),
+        ], "Cell positions are {:?}", edge_cell_positions);
+}
