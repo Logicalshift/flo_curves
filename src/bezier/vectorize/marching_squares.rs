7c3710331986b87102cf41321ad9c7206b152174
Andrew Hunter <andrew@logicalshift.co.uk>
2023-03-19 00:13:37 +0000

    First pass at following the edge loops

--- src/bezier/vectorize/marching_squares.rs
+++ src/bezier/vectorize/marching_squares.rs
@@ -1,6 +1,8 @@
 use super::sampled_contour::*;
 use crate::geo::*;
 
+use smallvec::*;
+
 use std::collections::{HashMap};
 
 ///
@@ -107,12 +109,12 @@ impl ContourCell {
 ///
 /// Uses the marching squares algorithm to trace the paths represented by a sampled contour
 ///
-pub fn trace_contours_from_samples<TCoord>(contours: impl SampledContour) /* -> impl Iterator<Item=Vec<TCoord>> */
+pub fn trace_contours_from_samples<TCoord>(contours: impl SampledContour) -> impl Iterator<Item=Vec<ContourEdge /* TCoord */>>
 where
     TCoord: Coordinate2D,
 {
     // Hash map indicating which edges are connected to each other
-    let mut edge_graph = HashMap::new();
+    let mut edge_graph = HashMap::<_, SmallVec<[_; 2]>>::new();
 
     // Create the graph of connected edges
     for (pos, cell) in contours.edge_cell_iterator() {
@@ -120,21 +122,60 @@ where
             ConnectedEdges::None => { }
 
             ConnectedEdges::One(ContourEdgePair(a, b)) => {
-                edge_graph.insert(a, b);
-                edge_graph.insert(b, a);
+                edge_graph.entry(a).or_insert_with(|| smallvec![]).push(b);
+                edge_graph.entry(b).or_insert_with(|| smallvec![]).push(a);
             }
 
             ConnectedEdges::Two(ContourEdgePair(a, b), ContourEdgePair(c, d)) => {
-                edge_graph.insert(a, b);
-                edge_graph.insert(b, a);
+                edge_graph.entry(a).or_insert_with(|| smallvec![]).push(b);
+                edge_graph.entry(b).or_insert_with(|| smallvec![]).push(a);
 
-                edge_graph.insert(c, d);
-                edge_graph.insert(d, c);
+                edge_graph.entry(c).or_insert_with(|| smallvec![]).push(d);
+                edge_graph.entry(d).or_insert_with(|| smallvec![]).push(c);
             }
         }
     }
 
-    // TODO: Remove connected edges to generate the resulting coordinates
+    // Graph contains a set of non-intersecting loops: process them into sets of points
+    let mut result = vec![];
 
-    todo!()
+    loop {
+        // We can fetch any edge from the hash table to follow it around in a loop
+        let (first_edge, next_edge) = if let Some((initial_edge, following_edges)) = edge_graph.iter().next() {
+            (*initial_edge, following_edges[0])
+        } else {
+            break;
+        };
+
+        // Follow the loop to generate the points
+        let mut edge_loop       = vec![first_edge];
+        let mut previous_edge   = first_edge;
+        let mut current_edge    = next_edge;
+
+        // We remove the edges from the graph as we process them so we don't read them again
+        edge_graph.remove(&first_edge);
+
+        while current_edge != first_edge {
+            // Add this edge to the loop
+            edge_loop.push(current_edge);
+
+            // Fetch the following edge (assumes we always generate full loops)
+            let following = edge_graph.remove(&current_edge).unwrap();
+            let next_edge = if following[0] != previous_edge {
+                following[0]
+            } else {
+                following[1]
+            };
+
+            // Move on to the next item
+            previous_edge   = current_edge;
+            current_edge    = next_edge;
+        }
+
+        // Store the loop in the result
+        result.push(edge_loop);
+    }
+
+    // Result is the final graph
+    result.into_iter()
 }
