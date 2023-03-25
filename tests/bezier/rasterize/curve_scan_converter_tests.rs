3f49e7bea5e44fa08abf656cb948dffc841403ae
Andrew Hunter <andrew@logicalshift.co.uk>
2023-03-25 22:06:55 +0000

    Test for out-of-bounds values for t

--- tests/bezier/rasterize/curve_scan_converter_tests.rs
+++ tests/bezier/rasterize/curve_scan_converter_tests.rs
@@ -35,7 +35,16 @@ fn wave_horizontal() {
     for p in wave_points {
         match p {
             ScanEdgeFragment::StartScanline(new_y)  => { y = new_y; },
-            ScanEdgeFragment::Edge(ScanX(x), _)     => { 
+            ScanEdgeFragment::Edge(ScanX(x), ScanFragment { t, .. })   => {
+                let t_point         = wave.point_at_pos(t);
+                let distance        = t_point.distance_to(&Coord2(x, y as f64));
+
+                println!("t = {}, p = {:?}", t, t_point);
+
+                assert!(distance < 0.01, "{:?} is {} units away from where it should be (actual point is {:?})", Coord2(x, y as f64), distance, t_point);
+                assert!(t >= 0.0, "t={} (negative numbers should not be in the results", t);
+                assert!(t <= 1.0, "t={} (>1 should not be in the results", t);
+
                 let closest_point   = wave.nearest_point(&Coord2(x, y as f64));
                 let distance        = closest_point.distance_to(&Coord2(x, y as f64));
                 assert!(distance < 0.01, "{:?} is {} units away from where it should be", Coord2(x, y as f64), distance);
@@ -59,14 +68,20 @@ fn wave_vertical() {
     let wave            = Curve::from_points(Coord2(10.0, 10.0), (Coord2(20.0, 30.0), Coord2(0.0, 50.0)), Coord2(10.0, 70.0));
     let scan_converter  = RootSolvingScanConverter::new(0..1000);
 
-    let mut wave_points = scan_converter.scan_convert(&wave);
+    let wave_points     = scan_converter.scan_convert(&wave);
 
     let mut square      = vec![' '; 80*80];
     let mut y           = 0;
     for p in wave_points {
         match p {
-            ScanEdgeFragment::StartScanline(new_y)  => { y = new_y; },
-            ScanEdgeFragment::Edge(ScanX(x), _)     => { 
+            ScanEdgeFragment::StartScanline(new_y)                      => { y = new_y; },
+            ScanEdgeFragment::Edge(ScanX(x), ScanFragment { t, .. })   => {
+                let t_point         = wave.point_at_pos(t);
+                let distance        = t_point.distance_to(&Coord2(x, y as f64));
+                assert!(distance < 0.01, "{:?} is {} units away from where it should be (actual point is {:?})", Coord2(x, y as f64), distance, t_point);
+                assert!(t >= 0.0, "t={} (negative numbers should not be in the results", t);
+                assert!(t <= 1.0, "t={} (>1 should not be in the results", t);
+
                 let closest_point   = wave.nearest_point(&Coord2(x, y as f64));
                 let distance        = closest_point.distance_to(&Coord2(x, y as f64));
                 assert!(distance < 0.01, "{:?} is {} units away from where it should be", Coord2(x, y as f64), distance);
