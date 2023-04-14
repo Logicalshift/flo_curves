1178bedf3589bb5d7f1bfd9d42b0f3fbd0cf7335
Andrew Hunter <andrew@logicalshift.co.uk>
2023-04-14 13:59:21 +0100

    Add a benchmark for the root solving scan converter

--- benches/rasterize.rs
+++ benches/rasterize.rs
@@ -1,9 +1,14 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 
 use flo_curves::arc::*;
+use flo_curves::bezier::*;
 use flo_curves::bezier::path::*;
 use flo_curves::bezier::rasterize::*;
-use flo_curves::geo::*;
+
+fn scan_convert_curve(curve: Curve<Coord2>) -> Vec<ScanEdgeFragment> {
+    let scan_converter = RootSolvingScanConverter::new(0..1000);
+    scan_converter.scan_convert(&curve).collect()
+}
 
 fn scan_convert_circle(path: &Vec<SimpleBezierPath>) -> Vec<ScanEdgeFragment> {
     let scan_converter = BezierPathScanConverter::new(0..1000);
@@ -11,11 +16,13 @@ fn scan_convert_circle(path: &Vec<SimpleBezierPath>) -> Vec<ScanEdgeFragment> {
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let radius      = 300.0;
-    let center      = Coord2(500.0, 500.0);
-    let circle_path = Circle::new(center, radius).to_path::<SimpleBezierPath>();
+    let radius          = 300.0;
+    let center          = Coord2(500.0, 500.0);
+    let circle_path     = Circle::new(center, radius).to_path::<SimpleBezierPath>();
+    let circle_curves   = circle_path.to_curves::<Curve<_>>();
 
     c.bench_function("scan_convert_circle", |b| b.iter(|| scan_convert_circle(&vec![circle_path.clone()])));
+    c.bench_function("scan_convert_curve", |b| b.iter(|| scan_convert_curve(circle_curves[0].clone())));
 }
 
 criterion_group!(benches, criterion_benchmark);
