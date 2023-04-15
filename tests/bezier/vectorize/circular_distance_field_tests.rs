2efc8d931907e44f7e8ef9435f9bca95d3a50dfb
Andrew Hunter <andrew@logicalshift.co.uk>
2023-04-15 22:42:38 +0100

    Add a test that compares the output of the iterator to the output of a bitmapped version of the same thing

---
+++ tests/bezier/vectorize/circular_distance_field_tests.rs
@@ -0,0 +1,23 @@
+use flo_curves::bezier::vectorize::*;
+
+#[test]
+fn contour_vs_bitmap() {
+    // Create a distance field contour
+    let contour = CircularDistanceField::with_radius(16.0);
+
+    // Use point_is_inside to generate a bitmap version of the contour
+    let bitmap = (0..(contour.size().0 * contour.size().1)).into_iter()
+        .map(|pos| (pos % contour.size().1, pos / contour.size().1))
+        .map(|(x, y)| contour.point_is_inside(ContourPosition(x, y)))
+        .collect::<Vec<_>>();
+    let bitmap = BoolSampledContour(contour.size(), bitmap);
+
+    // Get the edges from both
+    let contour_edges   = contour.edge_cell_iterator().collect::<Vec<_>>();
+    let bitmap_edges    = bitmap.edge_cell_iterator().collect::<Vec<_>>();
+
+    // Should generate identical results
+    assert!(contour_edges == bitmap_edges, "Bitmap edges were {}\n\nContour edges were {}", 
+        bitmap_edges.iter().map(|edge| format!("{:?}", edge)).collect::<Vec<_>>().join("\n  "),
+        contour_edges.iter().map(|edge| format!("{:?}", edge)).collect::<Vec<_>>().join("\n  "));
+}
--- tests/bezier/vectorize/mod.rs
+++ tests/bezier/vectorize/mod.rs
@@ -1,2 +1,3 @@
 mod sampled_contour_tests;
 mod marching_squares_tests;
+mod circular_distance_field_tests;
