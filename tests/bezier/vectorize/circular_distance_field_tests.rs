109486491119f98ca760994a89d219e8fa52e411
Andrew Hunter <andrew@logicalshift.co.uk>
2023-04-16 16:35:17 +0100

    Draw out the bitmap for the distance field tests

--- tests/bezier/vectorize/circular_distance_field_tests.rs
+++ tests/bezier/vectorize/circular_distance_field_tests.rs
@@ -10,6 +10,13 @@ fn contour_vs_bitmap_1() {
         .map(|pos| (pos % contour.size().1, pos / contour.size().1))
         .map(|(x, y)| contour.point_is_inside(ContourPosition(x, y)))
         .collect::<Vec<_>>();
+
+    for p in 0..bitmap.len() {
+        print!("{}", if bitmap[p] { '#' } else { '.' });
+        if ((p+1)%contour.size().1) == 0 { println!() };
+    }
+    println!();
+
     let bitmap = BoolSampledContour(contour.size(), bitmap);
 
     // Get the edges from both
@@ -17,7 +24,9 @@ fn contour_vs_bitmap_1() {
     let bitmap_edges    = bitmap.edge_cell_iterator().collect::<Vec<_>>();
 
     // Should generate identical results
-    assert!(contour_edges.len() == bitmap_edges.len(), "Returned different number of edges. Bitmap edges were \n  {}\n\nContour edges were \n  {}",
+    assert!(contour_edges.len() == bitmap_edges.len(), "Returned different number of edges ({} vs {}). Bitmap edges were \n  {}\n\nContour edges were \n  {}",
+        bitmap_edges.len(),
+        contour_edges.len(),
         bitmap_edges.iter().map(|edge| format!("{:?}", edge)).collect::<Vec<_>>().join("\n  "),
         contour_edges.iter().map(|edge| format!("{:?}", edge)).collect::<Vec<_>>().join("\n  "));
 
@@ -38,6 +47,13 @@ fn contour_vs_bitmap_2() {
         .map(|pos| (pos % contour.size().1, pos / contour.size().1))
         .map(|(x, y)| contour.point_is_inside(ContourPosition(x, y)))
         .collect::<Vec<_>>();
+
+    for p in 0..bitmap.len() {
+        print!("{}", if bitmap[p] { '#' } else { '.' });
+        if ((p+1)%contour.size().1) == 0 { println!() };
+    }
+    println!();
+
     let bitmap = BoolSampledContour(contour.size(), bitmap);
 
     // Get the edges from both
@@ -45,7 +61,9 @@ fn contour_vs_bitmap_2() {
     let bitmap_edges    = bitmap.edge_cell_iterator().collect::<Vec<_>>();
 
     // Should generate identical results
-    assert!(contour_edges.len() == bitmap_edges.len(), "Returned different number of edges. Bitmap edges were \n  {}\n\nContour edges were \n  {}",
+    assert!(contour_edges.len() == bitmap_edges.len(), "Returned different number of edges ({} vs {}). Bitmap edges were \n  {}\n\nContour edges were \n  {}",
+        bitmap_edges.len(),
+        contour_edges.len(),
         bitmap_edges.iter().map(|edge| format!("{:?}", edge)).collect::<Vec<_>>().join("\n  "),
         contour_edges.iter().map(|edge| format!("{:?}", edge)).collect::<Vec<_>>().join("\n  "));
 
@@ -66,6 +84,13 @@ fn contour_vs_bitmap_3() {
         .map(|pos| (pos % contour.size().1, pos / contour.size().1))
         .map(|(x, y)| contour.point_is_inside(ContourPosition(x, y)))
         .collect::<Vec<_>>();
+
+    for p in 0..bitmap.len() {
+        print!("{}", if bitmap[p] { '#' } else { '.' });
+        if ((p+1)%contour.size().1) == 0 { println!() };
+    }
+    println!();
+
     let bitmap = BoolSampledContour(contour.size(), bitmap);
 
     // Get the edges from both
@@ -73,7 +98,9 @@ fn contour_vs_bitmap_3() {
     let bitmap_edges    = bitmap.edge_cell_iterator().collect::<Vec<_>>();
 
     // Should generate identical results
-    assert!(contour_edges.len() == bitmap_edges.len(), "Returned different number of edges. Bitmap edges were \n  {}\n\nContour edges were \n  {}",
+    assert!(contour_edges.len() == bitmap_edges.len(), "Returned different number of edges ({} vs {}). Bitmap edges were \n  {}\n\nContour edges were \n  {}",
+        bitmap_edges.len(),
+        contour_edges.len(),
         bitmap_edges.iter().map(|edge| format!("{:?}", edge)).collect::<Vec<_>>().join("\n  "),
         contour_edges.iter().map(|edge| format!("{:?}", edge)).collect::<Vec<_>>().join("\n  "));
 
