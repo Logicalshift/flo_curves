5b28954d83305cc08db16bf1b28b4d25e7cce47c
Andrew Hunter <andrew@logicalshift.co.uk>
2023-04-16 12:25:13 +0100

    Add a test for the case where the radius is an integer but odd
    This fails like the 'integer but even' case but in a slightly different place, maybe a bit more understandably, it's possible for a 'grazing' intersection to have empty samples on both sides.

--- tests/bezier/vectorize/circular_distance_field_tests.rs
+++ tests/bezier/vectorize/circular_distance_field_tests.rs
@@ -25,6 +25,28 @@ fn contour_vs_bitmap_1() {
 #[test]
 fn contour_vs_bitmap_2() {
     // Create a distance field contour
+    let contour = CircularDistanceField::with_radius(15.0);
+
+    // Use point_is_inside to generate a bitmap version of the contour
+    let bitmap = (0..(contour.size().0 * contour.size().1)).into_iter()
+        .map(|pos| (pos % contour.size().1, pos / contour.size().1))
+        .map(|(x, y)| contour.point_is_inside(ContourPosition(x, y)))
+        .collect::<Vec<_>>();
+    let bitmap = BoolSampledContour(contour.size(), bitmap);
+
+    // Get the edges from both
+    let contour_edges   = contour.edge_cell_iterator().collect::<Vec<_>>();
+    let bitmap_edges    = bitmap.edge_cell_iterator().collect::<Vec<_>>();
+
+    // Should generate identical results
+    assert!(contour_edges == bitmap_edges, "Bitmap edges were {}\n\nContour edges were {}", 
+        bitmap_edges.iter().map(|edge| format!("{:?}", edge)).collect::<Vec<_>>().join("\n  "),
+        contour_edges.iter().map(|edge| format!("{:?}", edge)).collect::<Vec<_>>().join("\n  "));
+}
+
+#[test]
+fn contour_vs_bitmap_3() {
+    // Create a distance field contour
     let contour = CircularDistanceField::with_radius(16.1);
 
     // Use point_is_inside to generate a bitmap version of the contour
