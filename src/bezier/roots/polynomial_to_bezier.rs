421abd66ec3f7a7e15c84300c588dfd2467c941d
Andrew Hunter <andrew@logicalshift.co.uk>
2023-05-31 21:24:34 +0100

    Add an implementation of the polynomial conversion routine from Graphics Gems
    (I don't actually understand this right now, so it's the same algorithm as in the book)

--- src/bezier/roots/polynomial_to_bezier.rs
+++ src/bezier/roots/polynomial_to_bezier.rs
@@ -1,5 +1,7 @@
 use crate::geo::*;
 
+use std::convert::{TryInto};
+
 ///
 /// Generates the control polygon corresponding to a polynomial
 ///
@@ -10,6 +12,29 @@ where
     TPoint: Coordinate + Coordinate2D,
 {
     // See "A bezier curve-based root-finder", Philip J Schneider, Graphics Gems
-    
-    todo!()
+    let mut coefficients = coefficients;
+
+    for j in 1..=N {
+        let c       = 1.0 / (N as f64 + 1.0 - j as f64);
+        let mut d   = 1.0;
+        let mut e   = c;
+
+        for i in (j..=N).into_iter().rev() {
+            coefficients[i] = d * coefficients[i] + e * coefficients[i-1];
+            d = d - c;
+            e = e + c;
+        }
+    }
+
+    // Convert to points (range is 0..1)
+    let coefficients = coefficients.iter().enumerate().map(|(x, y)| {
+        let x = (x as f64) / (N as f64);
+        TPoint::from_components(&[x, *y])
+    }).collect::<Vec<_>>().try_into();
+
+    if let Ok(coefficients) = coefficients {
+        coefficients
+    } else {
+        unreachable!()
+    }
 }
