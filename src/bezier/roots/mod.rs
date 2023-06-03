69b08a517c223675435ad0ad5bb84f0ee161a41d
Andrew Hunter <andrew@logicalshift.co.uk>
2023-06-03 15:06:57 +0100

    Add an implementation of the nearest point algorithm from graphics gems

--- src/bezier/roots/mod.rs
+++ src/bezier/roots/mod.rs
@@ -2,6 +2,8 @@
 
 mod polynomial_to_bezier;
 mod find_roots;
+mod nearest_point_bezier_root_finder;
 
 pub use polynomial_to_bezier::*;
 pub use find_roots::*;
+pub (crate) use nearest_point_bezier_root_finder::*;
