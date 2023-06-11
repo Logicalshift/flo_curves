9546aa5c264bb0724a40d374e91c76d60eda88a3
Andrew Hunter <andrew@logicalshift.co.uk>
2023-06-11 18:04:00 +0100

    We can split out the time spent starting to iterate over the circles too

--- benches/vectorize.rs
+++ benches/vectorize.rs
@@ -32,6 +32,10 @@ fn sampled_circle(size: usize, radius: usize) -> BoolSampledContour {
     contour
 }
 
+fn start_edge_iteration<TContour: SampledContour>(contour: TContour) -> impl Iterator<Item=(ContourPosition, ContourCell)> {
+    contour.edge_cell_iterator()
+}
+
 fn find_edges<TContour: SampledContour>(contour: TContour) -> Vec<(ContourPosition, ContourCell)> {
     contour.edge_cell_iterator().collect()
 }
@@ -90,10 +94,12 @@ fn criterion_benchmark(c: &mut Criterion) {
 
     c.bench_function("circle_from_contours 100", |b| b.iter(|| circle_from_contours(&circle_100)));
     c.bench_function("circle_from_contours 1000", |b| b.iter(|| circle_from_contours(&circle_1000)));
+    c.bench_function("start_circle_iteration", |b| b.iter(|| start_edge_iteration(&circle_1000)));
     c.bench_function("circle_from_contours_not_sampled 100", |b| b.iter(|| circle_from_contours(&circle_100_generated)));
     c.bench_function("circle_from_contours_not_sampled 1000", |b| b.iter(|| circle_from_contours(&circle_1000_generated)));
 
     c.bench_function("create_brush_stroke_daubs", |b| b.iter(|| create_brush_stroke_daubs()));
+    c.bench_function("start_brush_iteration", |b| b.iter(|| start_edge_iteration(&daub_distance_field)));
     c.bench_function("read_brush_stroke_edges", |b| b.iter(|| find_edges(&daub_distance_field)));
     c.bench_function("read_edge_distances", |b| b.iter(|| read_edge_distances(&daub_distance_field, &distance_field_edges)));
     c.bench_function("trace_distance_field", |b| b.iter(|| trace_distance_field(&daub_distance_field)));
