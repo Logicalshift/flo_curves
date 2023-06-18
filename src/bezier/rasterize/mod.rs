f4d5c59aeef650f967b3ff00f4b6e41adc1c7432
Andrew Hunter <andrew@logicalshift.co.uk>
2023-06-18 12:19:28 +0100

    Add a ray cast contour implementation (and an empty path contour implementation)

--- src/bezier/rasterize/mod.rs
+++ src/bezier/rasterize/mod.rs
@@ -1,7 +1,10 @@
 mod scan_converter;
 mod curve_scan_converter;
 mod path_scan_converter;
+mod path_contour;
+mod ray_cast_contour;
 
 pub use scan_converter::*;
 pub use curve_scan_converter::*;
 pub use path_scan_converter::*;
+pub use ray_cast_contour::*;
---
+++ src/bezier/rasterize/path_contour.rs
@@ -0,0 +1,6 @@
+///
+/// Provides an implementation of the `SampledContour` interface for a bezier path
+///
+pub struct PathContour {
+
+}
