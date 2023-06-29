23f20a2ba269e7e180256e51e43c5b60a11ae4ed
Andrew Hunter <andrew@logicalshift.co.uk>
2023-06-29 20:47:17 +0100

    Some formatting

--- src/bezier/walk.rs
+++ src/bezier/walk.rs
@@ -12,7 +12,10 @@ use crate::geo::*;
 /// be spaced according to the shape of the curve (will have an uneven distance between them) 
 ///
 #[inline]
-pub fn walk_curve_unevenly<Curve: BezierCurve>(curve: &Curve, num_subdivisions: usize) -> impl '_ + Iterator<Item=CurveSection<'_, Curve>> {
+pub fn walk_curve_unevenly<Curve>(curve: &Curve, num_subdivisions: usize) -> impl '_ + Iterator<Item=CurveSection<'_, Curve>> 
+where
+    Curve: BezierCurve,
+{
     if num_subdivisions > 0 {
         UnevenWalkIterator {
             curve:              curve,
@@ -36,7 +39,10 @@ pub fn walk_curve_unevenly<Curve: BezierCurve>(curve: &Curve, num_subdivisions: 
 /// This walks evenly using the curve's chord length rather than the arc length: each section returned will have a `chord_length()`
 /// of `distance`. The call `vary_by()` can be used on the result to vary the step size at each point.
 ///
-pub fn walk_curve_evenly<Curve: BezierCurve>(curve: &Curve, distance: f64, max_error: f64) -> EvenWalkIterator<'_, Curve> {
+pub fn walk_curve_evenly<Curve>(curve: &Curve, distance: f64, max_error: f64) -> EvenWalkIterator<'_, Curve> 
+where
+    Curve: BezierCurve,
+{
     const INITIAL_INCREMENT: f64 = 0.01;
 
     // Too small or negative values might produce bad effects due to floating point inprecision
