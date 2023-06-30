6aaf12b3b580fbee232d915a645c4b17bef93749
Andrew Hunter <andrew@logicalshift.co.uk>
2023-07-01 00:14:11 +0100

    Add a test for the approximate distance field cache
    I was expecting it to pass the 'inside/outside' test with enough points on the circle but this is still letting 'outside' points in.

---
+++ tests/bezier/rasterize/sampled_approx_distance_field_cache_tests.rs
@@ -0,0 +1,74 @@
+use flo_curves::geo::*;
+use flo_curves::bezier::rasterize::*;
+use flo_curves::bezier::vectorize::*;
+
+use std::f64;
+
+///
+/// Creates a cache containing a circle of the specified radius, with a center at (radius+1, radius+1)
+///
+fn create_circle_sample(num_points: usize, radius: f64) -> SampledApproxDistanceFieldCache {
+    // Size should encompass the whole circle
+    let size = (radius+1.0) * 2.0;
+    let size = size.ceil() as usize;
+
+    // Create num_points samples around the perimeter of the circle
+    let center = Coord2(radius+1.0, radius+1.0);
+    let points = (0..num_points)
+        .map(|t| {
+            let t = (t as f64)/(num_points as f64);
+            let t = 2.0 * f64::consts::PI * t;
+            let x = radius * t.sin();
+            let y = radius * t.cos();
+
+            Coord2(x, y) + center
+        });
+
+    // Need an 'is_inside' function
+    let is_inside = move |x, y| {
+        let pos = Coord2(x, y);
+        let pos = pos - center;
+
+        let distance_sq = pos.dot(&pos);
+
+        distance_sq < (radius * radius)
+    };
+
+    // Generate a cache
+    SampledApproxDistanceFieldCache::from_points(points, is_inside, ContourSize(size, size))
+}
+
+fn check_circle_distances(cache: &mut SampledApproxDistanceFieldCache, radius: f64) {
+    // Fetch the size and expected center point
+    let size    = cache.size();
+    let center  = Coord2(radius+1.0, radius+1.0);
+
+    // Iterate over every coordinate
+    for y in 0..size.height() {
+        for x in 0..size.height() {
+            // Request the distance squared
+            let distance_squared = cache.distance_squared_at_point(ContourPosition(x, y));
+
+            // Interpret it as inside and convert it to a 'real' distance
+            let is_inside   = distance_squared < 0.0;
+            let distance    = distance_squared.abs().sqrt();
+
+            // Compute the expected values
+            let actual_pos = Coord2(x as _, y as _);
+            let circle_pos = actual_pos - center;
+
+            let from_center = circle_pos.dot(&circle_pos).sqrt();
+            let from_edge   = from_center - radius;
+
+            debug_assert!((from_edge.abs() - distance.abs()).abs() < 4.0, "({}, {}) has distance {} but was expecting {}", x, y, from_edge.abs(), distance.abs());
+
+            debug_assert!(!is_inside || from_edge <= 0.0, "({}, {}) should be outside the circle but isn't. Distance {} (approximated as {})", x, y, from_edge, distance);
+            debug_assert!(is_inside || from_edge >= 0.0, "({}, {}) should be inside the circle but isn't. Distance {} (approximated as {})", x, y, from_edge, distance);
+        }
+    } 
+}
+
+#[test]
+fn circle_100_many_points() {
+    check_circle_distances(&mut create_circle_sample(4000, 100.0), 100.0);
+}
