68bd6b357a69844241e256e8d7f34c9a2bf9c1dc
Andrew Hunter <andrew@logicalshift.co.uk>
2023-07-02 15:33:09 +0100

    Just load the samples directly from the list without looking at the surrounding points
    (This makes things fail again but I think that's fixable by moving the 'look at surrounding points' further out)

--- src/bezier/rasterize/sampled_approx_distance_field_cache.rs
+++ src/bezier/rasterize/sampled_approx_distance_field_cache.rs
@@ -49,9 +49,6 @@ impl SampledApproxDistanceFieldCache {
     {
         let interior_pixels = interior_pixels.into_iter().collect();
 
-        let width   = size.width() as isize;
-        let height  = size.height() as isize;
-
         // The zero points are used as reference points
         let zero_points = perimeter_samples.into_iter()
             .map(|(pos, point)| (pos, point.x(), point.y()))
@@ -63,46 +60,24 @@ impl SampledApproxDistanceFieldCache {
         let mut waiting_points  = HashSet::new();
 
         for idx in 0..zero_points.len() {
-            // Fetch the next point
             let (nearest_point, sample_x, sample_y) = zero_points[idx];
 
-            // Measure the distances for the points around the current point
-            for y_offset in -1..=1 {
-                // Use the offset point
-                let point_y = (nearest_point.y() as isize) + y_offset;
-                if point_y < 0 || point_y >= height {
-                    continue;
+            // Work out the distance to this point
+            let (point_x, point_y)      = (nearest_point.x() as f64, nearest_point.y() as f64);
+            let (offset_x, offset_y)    = (sample_x - point_x, sample_y - point_y);
+            let distance_squared        = offset_x*offset_x + offset_y*offset_y;
+
+            // Add or update the point in the cache
+            if let Some((existing_distance, existing_idx)) = cached_points.get_mut(&nearest_point) {
+                // Replace the existing point with this one if it's nearer to an edge
+                if distance_squared < *existing_distance {
+                    *existing_distance  = distance_squared;
+                    *existing_idx       = idx;
                 }
-
-                for x_offset in -1..=1 {
-                    // Use the offset point (ignore samples outside of the size boundary)
-                    let point_x = (nearest_point.x() as isize) + x_offset;
-                    if point_x < 0 || point_x >= width {
-                        continue;
-                    }
-
-                    // Sample this position, determine if it's inside or not
-                    let pos         = ContourPosition(point_x as usize, point_y as usize);
-
-                    let offset_x    = sample_x - (pos.0 as f64);
-                    let offset_y    = sample_y - (pos.1 as f64);
-                    let distance    = offset_x*offset_x + offset_y*offset_y;
-
-                    // Update the cache at this point
-                    if let Some((existing_distance, existing_idx)) = cached_points.get_mut(&pos) {
-                        // Replace the existing point if this one is closer
-                        if distance < existing_distance.abs() {
-                            *existing_distance  = distance;
-                            *existing_idx       = idx;
-                        }
-                    } else {
-                        // Haven't seen this point yet, so this is the closest perimeter point to it
-                        cached_points.insert(pos, (distance, idx));
-
-                        // As we haven't seen this point before, add to the waiting points
-                        waiting_points.insert(pos);
-                    }
-                }
+            } else {
+                // Add a new point
+                cached_points.insert(nearest_point, (distance_squared, idx));
+                waiting_points.insert(nearest_point);
             }
         }
 
