5f6bcdc5a5c8e5b2e73240ad5b04812847ddb6c5
Andrew Hunter <andrew@logicalshift.co.uk>
2023-07-02 12:42:17 +0100

    Treat the point that we find as a guess and use newton-raphson to find a closer guess for the path
    (This does seem to have a positive impact but still doesn't produce a very smooth result for trace_circle)

--- src/bezier/rasterize/path_distance_field.rs
+++ src/bezier/rasterize/path_distance_field.rs
@@ -28,8 +28,8 @@ impl PathDistanceField {
         // Generate the distance field cache: need to walk the perimeter of the curve to find evenly-spaced points
         let curves      = path.iter().flat_map(|subpath| subpath.to_curves::<Curve<_>>()).collect::<Vec<_>>();
         let points      = curves.iter().enumerate()
-            .flat_map(|(curve_idx, curve)| walk_curve_evenly_map(*curve, 1.0, 0.1, |section| section.point_at_pos(1.0)))
-            .map(|point| (ContourPosition(point.x().round() as _, point.y().round() as _), point));
+            .flat_map(|(curve_idx, curve)| walk_curve_evenly_map(*curve, 0.5, 0.1, move |section| (curve_idx, section.point_at_pos(1.0), section.t_for_t(1.0))))
+            .flat_map(|(curve_idx, point_guess, t_guess)| refine_closest_point(&curves[curve_idx], point_guess, t_guess));
 
         // The path contour can be used both as the actual path contour and as a way to determine if a point is inside the path
         let path_contour = PathContour::from_path(path, size);
@@ -40,6 +40,34 @@ impl PathDistanceField {
         let approx_distance_field = RefCell::new(approx_distance_field);
 
         PathDistanceField { path_contour, approx_distance_field }
+    }
+}
+
+///
+/// Takes a known point on the curve, and uses it to refine a nearby point that's exactly on the grid and close to this point
+///
+fn refine_closest_point<TPoint>(curve: &Curve<TPoint>, point_guess: TPoint, t_guess: f64) -> Option<(ContourPosition, TPoint)>
+where
+    TPoint: Coordinate + Coordinate2D,
+{
+    // Grid position is the rounded version of the point
+    let grid_x = point_guess.x().round();
+    let grid_y = point_guess.y().round();
+
+    if grid_x < 0.0 || grid_y < 0.0 {
+        // Outside of the bounds of the path
+        None
+    } else {
+        // Grid position
+        let grid_position = ContourPosition(grid_x as _, grid_y as _);
+
+        // Create a refined t value using a few rounds of newton-raphson that's near to the grid point
+        let refined_t = nearest_point_on_curve_newton_raphson_with_estimate(curve, &TPoint::from_components(&[grid_x, grid_y]), t_guess, 3);
+
+        // Generate this as the nearest point
+        let nearest_point = curve.point_at_pos(refined_t);
+
+        Some((grid_position, nearest_point))
     }
 }
 
