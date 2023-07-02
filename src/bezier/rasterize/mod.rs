ef3fb863c057cf22f546b18b3f407b23ace245b2
Andrew Hunter <andrew@logicalshift.co.uk>
2023-07-02 20:44:28 +0100

    Remove the scan converter (it's redundant as you can use the path contour or distance field to do the same job)

--- src/bezier/rasterize/mod.rs
+++ src/bezier/rasterize/mod.rs
@@ -1,14 +1,8 @@
-mod scan_converter;
-mod curve_scan_converter;
-mod path_scan_converter;
 mod path_contour;
 mod path_distance_field;
 mod ray_cast_contour;
 mod sampled_approx_distance_field_cache;
 
-pub use scan_converter::*;
-pub use curve_scan_converter::*;
-pub use path_scan_converter::*;
 pub use ray_cast_contour::*;
 pub use path_contour::*;
 pub use path_distance_field::*;
--- tests/bezier/rasterize/mod.rs
+++ tests/bezier/rasterize/mod.rs
@@ -1,5 +1,3 @@
-mod curve_scan_converter_tests;
-mod path_scan_converter_tests;
 mod ray_cast_contour_tests;
 mod path_contour_tests;
 mod sampled_approx_distance_field_cache_tests;
