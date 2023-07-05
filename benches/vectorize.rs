4025dc8320f6f0941384387acbcc3dc2671fe690
Andrew Hunter <andrew@logicalshift.co.uk>
2023-07-05 09:14:11 +0100

    Add a path brush tracing benchmark

--- benches/vectorize.rs
+++ benches/vectorize.rs
@@ -110,7 +110,10 @@ fn read_edge_distances(daubs: &DaubBrushDistanceField<CircularDistanceField>, ed
     }
 }
 
-fn trace_distance_field(daubs: &DaubBrushDistanceField<CircularDistanceField>) {
+fn trace_distance_field<TDistanceField>(daubs: &TDistanceField) 
+where
+    TDistanceField: SampledSignedDistanceField
+{
     trace_paths_from_distance_field::<SimpleBezierPath>(daubs, 1.0);
 }
 
@@ -187,6 +190,22 @@ fn criterion_benchmark(c: &mut Criterion) {
     }));
     c.bench_function("full_distance_field_small_brush", |b| b.iter(|| {
         let daub_distance_field = DaubBrushDistanceField::from_daubs(create_brush_stroke_daubs(20.0));
+        trace_distance_field(&daub_distance_field)
+    }));
+    c.bench_function("full_distance_field_path_brush", |b| b.iter(|| {
+        let radius          = 32.0;
+        let center          = Coord2(radius+1.0, radius+1.0);
+        let circle_path     = Circle::new(center, radius).to_path::<SimpleBezierPath>();
+        let circle_field    = PathDistanceField::from_path(vec![circle_path], ContourSize(1000, 1000));
+
+        let brush           = ScaledBrush::from_distance_field(&circle_field);
+
+        let brush_curve      = create_brush_stroke(20.0);
+        let brush            = &brush;
+        let (daubs, _offset) = brush_stroke_daubs_from_curve(&brush, &brush_curve, 0.5, 0.25);
+
+        let daub_distance_field = DaubBrushDistanceField::from_daubs(daubs);
+
         trace_distance_field(&daub_distance_field)
     }));
 }
