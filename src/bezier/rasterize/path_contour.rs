da53f809317e9c8ef69332d315b1972da17e59ae
Andrew Hunter <andrew@logicalshift.co.uk>
2023-07-07 21:29:52 +0100

    Remove the ray-cast contour from PathContour and calculate intercepts directly instead, add an empty implementation of the new column function

--- src/bezier/rasterize/path_contour.rs
+++ src/bezier/rasterize/path_contour.rs
@@ -12,7 +12,11 @@ use std::ops::{Range};
 /// Provides an implementation of the `SampledContour` interface for a bezier path
 ///
 pub struct PathContour {
-    contour: DynRayCastContour
+    /// The size of this contour
+    size: ContourSize,
+
+    /// The curves in the path
+    curves: Vec<(Curve<f64>, Curve<f64>, Bounds<Coord2>)>,
 }
 
 impl PathContour {
@@ -29,7 +33,8 @@ impl PathContour {
         let curves = path.iter()
             .flat_map(|path| path.to_curves::<Curve<_>>())
             .map(|curve| {
-                let bounds                  = curve.bounding_box::<Bounds<_>>();
+                let Bounds(min, max)        = curve.bounding_box::<Bounds<_>>();
+                let bounds                  = Bounds(Coord2::from_coordinate(min), Coord2::from_coordinate(max));
                 let (sp, (cp1, cp2), ep)    = curve.all_points();
 
                 let curve_x = Curve::from_points(sp.x(), (cp1.x(), cp2.x()), ep.x());
@@ -38,36 +43,8 @@ impl PathContour {
             })
             .collect::<Vec<_>>();
 
-        // Generate a ray-cast con
-        let contour = DynRayCastContour::new(Box::new(move |y| {
-                // Iterate through all of the curves to find the intercepts
-                let mut intercepts = vec![];
-
-                for (curve_x, curve_y, bounding_box) in curves.iter() {
-                    // Skip curves if they don't intercept this contour
-                    if y < bounding_box.min().y() || y > bounding_box.max().y() { continue; }
-
-                    // Solve the intercepts on the y axis
-                    let (w1, (w2, w3), w4)  = curve_y.all_points();
-                    let curve_intercepts    = solve_basis_for_t(w1, w2, w3, w4, y);
-
-                    // Add the intercepts to the list that we've been generating (we ignore t=0 as there should be a corresponding intercept at t=1 on the previous curve)
-                    intercepts.extend(curve_intercepts.into_iter().filter(|t| *t > 0.0).map(|t| curve_x.point_at_pos(t)));
-                }
-
-                // Order the intercepts to generate ranges
-                intercepts.sort_unstable_by(|a, b| a.total_cmp(b));
-
-                // Each tuple represents a range that is within the shape
-                return intercepts.into_iter()
-                    .tuples()
-                    .map(|(start, end)| start..end)
-                    .collect();
-            }),
-            size);
-
         PathContour { 
-            contour
+            curves, size,
         }
     }
 }
@@ -75,11 +52,42 @@ impl PathContour {
 impl SampledContour for PathContour {
     #[inline]
     fn contour_size(&self) -> ContourSize {
-        (&self.contour).contour_size()
+        self.size
     }
 
     #[inline]
     fn intercepts_on_line(&self, y: f64) -> SmallVec<[Range<f64>; 4]> {
-        (&self.contour).intercepts_on_line(y)
+        raycast_intercepts_on_line(&|y| {
+            // Iterate through all of the curves to find the intercepts
+            let mut intercepts = vec![];
+
+            for (curve_x, curve_y, bounding_box) in self.curves.iter() {
+                // Skip curves if they don't intercept this contour
+                if y < bounding_box.min().y() || y > bounding_box.max().y() { continue; }
+
+                // Solve the intercepts on the y axis
+                let (w1, (w2, w3), w4)  = curve_y.all_points();
+                let curve_intercepts    = solve_basis_for_t(w1, w2, w3, w4, y);
+
+                // Add the intercepts to the list that we've been generating (we ignore t=0 as there should be a corresponding intercept at t=1 on the previous curve)
+                intercepts.extend(curve_intercepts.into_iter().filter(|t| *t > 0.0).map(|t| curve_x.point_at_pos(t)));
+            }
+
+            // Order the intercepts to generate ranges
+            intercepts.sort_unstable_by(|a, b| a.total_cmp(b));
+
+            // Each tuple represents a range that is within the shape
+            return intercepts.into_iter()
+                .tuples()
+                .map(|(start, end)| start..end)
+                .collect();
+        }, y, 1.0, self.size.width())
+    }
+}
+
+impl ColumnSampledContour for PathContour {
+    #[inline]
+    fn intercepts_on_column(&self, x: f64) -> SmallVec<[Range<f64>; 4]> {
+        todo!()
     }
 }
