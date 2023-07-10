cba1624805caa68dcb68de8714d82d2eb2acd5eb
Andrew Hunter <andrew@logicalshift.co.uk>
2023-07-10 21:43:51 +0100

    Add the offset instead of subtracting it

--- demos/brush/src/main.rs
+++ demos/brush/src/main.rs
@@ -23,9 +23,9 @@ fn slow_distance_field_from_path(path: Vec<SimpleBezierPath>) -> F64SampledDista
 
     // Create the distance field by slowly measuring the path at every point
     let distance_field = create_distance_field(|x, y| {
-        let is_inside = path_contains_point(&path[0], &(Coord2(x, y)-offset));
+        let is_inside = path_contains_point(&path[0], &(Coord2(x, y)+offset));
         let distance  = path.iter()
-            .map(|subpath| path_closest_point(subpath, &(Coord2(x, y)-offset)))
+            .map(|subpath| path_closest_point(subpath, &(Coord2(x, y)+offset)))
             .map(|(_, _, distance, _)| distance)
             .reduce(|a, b| {
                 if a < b { a } else { b }
