9bb9812c0b52d901bb7062b04fd679c5334fd3c2
Andrew Hunter <andrew@logicalshift.co.uk>
2023-09-17 12:55:40 +0100

    Use path_intersects_ray and compare it to our contour intersection, plus use it to try to figure out if an intersection is actually inside the path

--- tests/bezier/rasterize/path_contour_tests.rs
+++ tests/bezier/rasterize/path_contour_tests.rs
@@ -1,4 +1,5 @@
 use flo_curves::arc::*;
+use flo_curves::line::*;
 use flo_curves::bezier::*;
 use flo_curves::bezier::path::*;
 use flo_curves::bezier::rasterize::*;
@@ -361,7 +362,29 @@ pub fn intercepts_1() {
     for y_pos in path.to_curves::<Curve<_>>().into_iter().map(|curve| curve.start_point().y()) {
         // Forward
         println!("y={} ({})", y_pos, (y_pos/100.0)-1.0);
-        let intercepts  = contour.intercepts_on_line(y_pos - offset.y());
+
+        let intercepts      = contour.intercepts_on_line(y_pos - offset.y());
+
+        let ray             = (Coord2(0.0, y_pos), Coord2(1.0, y_pos));
+        let ray_intercepts  = path_intersects_ray(&path, &ray).collect::<Vec<_>>();
+
+        println!("  Contour: {:?}\n  Ray-intercepts {:?}", intercepts, ray_intercepts);
+
+        // Check each intercept represents a section inside the path
+        for intercept in intercepts.iter() {
+            let mid_point       = (intercept.start + intercept.end) / 2.0;
+            let ray             = (Coord2(mid_point, 0.0), Coord2(mid_point, 1.0));
+            let mid_intercepts  = path_intersects_ray(&path, &ray)
+                .sorted_by(|(_, _, line_t1), (_, _, line_t2)| line_t1.total_cmp(line_t2))
+                .collect::<Vec<_>>();
+
+            let count_to_mid    = mid_intercepts.iter()
+                .filter(|(_, _, line_t)| { ray.point_at_pos(*line_t).y() <= y_pos })
+                .count();
+
+            println!("    {:?}", mid_intercepts);
+            //assert!(count_to_mid%2 == 1, "Intercept seems to create a region outside of the path x={} y={} ({}) ({:?})", mid_point, y_pos, (y_pos/100.0)-1.0, mid_intercepts);
+        }
 
         //assert!(intercepts.len()%2 == 0, "Uneven number of intercepts at ypos {:?} {:?} ({:?})", y_pos, (y_pos/100.0)-1.0, intercepts);
     }
