272e5fe5a3770b0b366972d43de784a6e87bd38c
Andrew Hunter <andrew@logicalshift.co.uk>
2024-07-08 21:38:03 +0100

    Use the original input row to decide whether a distance is inside or outside

--- src/bezier/rasterize/marching_parabola_distance_field.rs
+++ src/bezier/rasterize/marching_parabola_distance_field.rs
@@ -134,7 +134,9 @@ impl MarchingParabolaDistanceField {
                         ]),
                         |a, b| a.xpos < b.xpos),
                     (0..width).map(|x| x as f64))
-                .map(|DistanceSquared(distance)| distance);
+                .map(|DistanceSquared(distance)| distance)
+                .zip(input_row)
+                .map(|(val, original)| if original < &0.0 { -val } else { val });
 
             squared_distance_field.extend(marching_parabolas);
         }
