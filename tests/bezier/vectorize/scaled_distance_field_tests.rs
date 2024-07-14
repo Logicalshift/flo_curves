1a9f93c990efab9784b905723460b3cf171535bf
Andrew Hunter <andrew@logicalshift.co.uk>
2024-07-14 17:40:44 +0100

    Not sure why debug_assert is being used here

--- tests/bezier/vectorize/scaled_distance_field_tests.rs
+++ tests/bezier/vectorize/scaled_distance_field_tests.rs
@@ -14,7 +14,7 @@ fn trace_half_circle_sampled() {
     let half_contour    = ScaledContour::from_contour(&circle_contour, 0.5, (0.0, 0.0));
     let traced_circle   = trace_paths_from_samples::<SimpleBezierPath>(&half_contour, 2.0);
 
-    debug_assert!(traced_circle.len() == 1);
+    assert!(traced_circle.len() == 1);
 
     let mut num_points = 0;
     for curve in traced_circle[0].to_curves::<Curve<_>>() {
@@ -26,11 +26,11 @@ fn trace_half_circle_sampled() {
 
             let distance    = point.distance_to(&Coord2(251.0, 251.0));
 
-            debug_assert!((distance - (radius/2.0)) < 2.0, "Point #{} at distance {:?}", num_points, distance);
+            assert!((distance - (radius/2.0)) < 2.0, "Point #{} at distance {:?}", num_points, distance);
         }
     }
 
-    debug_assert!(traced_circle[0].to_curves::<Curve<_>>().len() < 32, "Result has {} curves", traced_circle[0].to_curves::<Curve<_>>().len());
+    assert!(traced_circle[0].to_curves::<Curve<_>>().len() < 32, "Result has {} curves", traced_circle[0].to_curves::<Curve<_>>().len());
 }
 
 #[test]
@@ -43,7 +43,7 @@ fn trace_half_circle() {
     let half_field      = ScaledDistanceField::from_distance_field(&circle_field, 0.5, (0.0, 0.0));
     let traced_circle   = trace_paths_from_distance_field::<SimpleBezierPath>(&half_field, 0.1);
 
-    debug_assert!(traced_circle.len() == 1);
+    assert!(traced_circle.len() == 1);
 
     let mut num_points = 0;
     for curve in traced_circle[0].to_curves::<Curve<_>>() {
@@ -55,11 +55,11 @@ fn trace_half_circle() {
 
             let distance    = point.distance_to(&Coord2(251.0, 251.0));
 
-            debug_assert!((distance - (radius/2.0)) < 0.3, "Point #{} at distance {:?}", num_points, distance);
+            assert!((distance - (radius/2.0)) < 0.3, "Point #{} at distance {:?}", num_points, distance);
         }
     }
 
-    debug_assert!(traced_circle[0].to_curves::<Curve<_>>().len() < 32, "Result has {} curves", traced_circle[0].to_curves::<Curve<_>>().len());
+    assert!(traced_circle[0].to_curves::<Curve<_>>().len() < 32, "Result has {} curves", traced_circle[0].to_curves::<Curve<_>>().len());
 }
 
 #[test]
@@ -72,7 +72,7 @@ fn trace_half_circle_offset() {
     let half_field      = ScaledDistanceField::from_distance_field(&circle_field, 0.5, (0.3, 0.4));
     let traced_circle   = trace_paths_from_distance_field::<SimpleBezierPath>(&half_field, 0.1);
 
-    debug_assert!(traced_circle.len() == 1);
+    assert!(traced_circle.len() == 1);
 
     let mut num_points = 0;
     for curve in traced_circle[0].to_curves::<Curve<_>>() {
@@ -84,11 +84,11 @@ fn trace_half_circle_offset() {
 
             let distance    = point.distance_to(&Coord2(251.3, 251.4));
 
-            debug_assert!((distance - (radius/2.0)) < 0.3, "Point #{} at distance {:?}", num_points, distance);
+            assert!((distance - (radius/2.0)) < 0.3, "Point #{} at distance {:?}", num_points, distance);
         }
     }
 
-    debug_assert!(traced_circle[0].to_curves::<Curve<_>>().len() < 32, "Result has {} curves", traced_circle[0].to_curves::<Curve<_>>().len());
+    assert!(traced_circle[0].to_curves::<Curve<_>>().len() < 32, "Result has {} curves", traced_circle[0].to_curves::<Curve<_>>().len());
 }
 
 #[test]
@@ -101,7 +101,7 @@ fn trace_third_circle() {
     let half_field      = ScaledDistanceField::from_distance_field(&circle_field, 1.0/3.0, (0.0, 0.0));
     let traced_circle   = trace_paths_from_distance_field::<SimpleBezierPath>(&half_field, 0.1);
 
-    debug_assert!(traced_circle.len() == 1);
+    assert!(traced_circle.len() == 1);
 
     let mut num_points = 0;
     for curve in traced_circle[0].to_curves::<Curve<_>>() {
@@ -113,11 +113,11 @@ fn trace_third_circle() {
 
             let distance    = point.distance_to(&Coord2(500.0 / 3.0 + 1.0, 500.0 / 3.0 + 1.0));
 
-            debug_assert!((distance - (radius/3.0)) < 1.0, "Point #{} at distance {:?}", num_points, distance);
+            assert!((distance - (radius/3.0)) < 1.0, "Point #{} at distance {:?}", num_points, distance);
         }
     }
 
-    debug_assert!(traced_circle[0].to_curves::<Curve<_>>().len() < 32, "Result has {} curves", traced_circle[0].to_curves::<Curve<_>>().len());
+    assert!(traced_circle[0].to_curves::<Curve<_>>().len() < 32, "Result has {} curves", traced_circle[0].to_curves::<Curve<_>>().len());
 }
 
 #[test]
@@ -130,7 +130,7 @@ fn trace_circle_and_a_half() {
     let bigger_field    = ScaledDistanceField::from_distance_field(&circle_field, 1.5, (0.0, 0.0));
     let traced_circle   = trace_paths_from_distance_field::<SimpleBezierPath>(&bigger_field, 0.1);
 
-    debug_assert!(traced_circle.len() == 1);
+    assert!(traced_circle.len() == 1);
 
     let mut num_points = 0;
     for curve in traced_circle[0].to_curves::<Curve<_>>() {
@@ -142,9 +142,9 @@ fn trace_circle_and_a_half() {
 
             let distance    = point.distance_to(&Coord2(751.0, 751.0));
 
-            debug_assert!((distance - (radius*1.5)) < 1.0, "Point #{} at distance {:?}", num_points, distance);
+            assert!((distance - (radius*1.5)) < 1.0, "Point #{} at distance {:?}", num_points, distance);
         }
     }
 
-    debug_assert!(traced_circle[0].to_curves::<Curve<_>>().len() < 32, "Result has {} curves", traced_circle[0].to_curves::<Curve<_>>().len());
+    assert!(traced_circle[0].to_curves::<Curve<_>>().len() < 32, "Result has {} curves", traced_circle[0].to_curves::<Curve<_>>().len());
 }
