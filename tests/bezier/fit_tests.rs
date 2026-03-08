f51ce96198611d368b5891592481cf94d79873d6
Andrew Hunter <andrew@logicalshift.co.uk>
2026-03-08 17:36:09 +0000

    The curve we were using for the test was degenerate so might not be a good example: add another test with a non-degenerate curve

--- tests/bezier/fit_tests.rs
+++ tests/bezier/fit_tests.rs
@@ -2,6 +2,18 @@ use flo_curves::bezier::*;
 
 #[test]
 fn fit_basic_curve() {
+    let curve       = Curve::from_points(Coord2(412.0, 500.0), (Coord2(442.0, 520.0), Coord2(163.0, 504.0)), Coord2(308.0, 665.0));
+    let points      = (0..=100).map(|t| t as f64/100.0).map(|t| curve.point_at_pos(t)).collect::<Vec<_>>();
+    let fit_curve   = fit_curve::<Curve<Coord2>>(&points, 0.01);
+
+    assert!(fit_curve.is_some());
+    let fit_curve = fit_curve.unwrap();
+
+    assert!(fit_curve.len() == 1, "{} curves: {:?}", fit_curve.len(), fit_curve);
+}
+
+#[test]
+fn fit_basic_curve_degenerate() {
     let curve       = Curve::from_points(Coord2(412.0, 500.0), (Coord2(412.0, 500.0), Coord2(163.0, 504.0)), Coord2(308.0, 665.0));
     let points      = (0..=100).map(|t| t as f64/100.0).map(|t| curve.point_at_pos(t)).collect::<Vec<_>>();
     let fit_curve   = fit_curve::<Curve<Coord2>>(&points, 0.01);
